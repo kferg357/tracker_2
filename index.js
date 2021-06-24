5db14e087b9aa338c27b4b8a2835161c4f400cfb
kferg357 <kennethferguson90@gmail.com>
2021-06-24 15:21:08 -0400

    added more index.js

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ connection.connect(function(err) {
     if (err) throw err;
     startUp();
 });
+
+function startUo() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            messages: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View ALl Departments",
+                "View All Roles",
+                "Add Employees",
+                "Add Departments",
+                "Add Roles",
+                "Update Employee Role",
+                "EXIT"
+            ]
+        })
+}
