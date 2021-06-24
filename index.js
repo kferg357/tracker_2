7af37e60e514a72dbaaf2311853972aafb83a06b
kferg357 <kennethferguson90@gmail.com>
2021-06-24 13:51:30 -0400

    added create connection

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Kenndall2017!",
+    database:"employees"
+});
